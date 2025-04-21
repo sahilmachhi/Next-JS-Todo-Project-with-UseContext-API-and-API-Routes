8f37857f69bf65be82c917ede717e67ec993eab2
Sahil <113676978+whonix2904@users.noreply.github.com>
2025-04-21 23:54:10 +0530

    fixed navbar transparent issue

--- todo-app/src/components/Navbar.tsx
+++ todo-app/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className='border-b shadow-md top-0 sticky'>
+        <div className='border-b shadow-md top-0 sticky z-50 bg-white'>
             <div className='container mx-auto w-full flex items-center justify-between py-4 px-4'>
                 <Link prefetch={true} href='/'>
                     <span className='text-2xl font-bold'>Todo App</span>
