489dc99b83d793efb7d1e0b2d7093850cc7f49d6
Sahil <113676978+whonix2904@users.noreply.github.com>
2025-04-22 13:05:46 +0530

    Forced the "/" page to be dynamic because it was caching data after some time post-deployment

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,6 +1,8 @@
 import AddTodoButton from "@/components/AddTodoButton";
 import TodoList from "@/components/TodoList";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0
 
 export default function Home() {
 
